363421ee15c5724121a637f8a36166e9c2f50f1d
Gaëtan Gilbert <gaetan.gilbert@ens-lyon.fr>
2017-07-25 00:01:22 +0200

    Add win chance script

--- src/lib.js
+++ src/lib.js
@@ -228,3 +228,86 @@ function get_doubletime_strat(states, s) {
     }
     return strat;
 }
+
+function get_winchance(wins, state) {
+    var rep = game_state_rep(state);
+    var reg = new RegExp ("" + rep + ":(.{1,4})");
+
+    var matches = wins.match(reg);
+    if (matches == null || matches.length < 2) {
+        console.log("N1 win chances: unknown state " + JSON.stringify(state) + " rep " + rep);
+        return "-1";
+    }
+    return matches[1];
+}
+
+// parsing used to be based on https://greasyfork.org/en/scripts/18136-n1bot-inputs
+
+/* A game is displayed in a big <td> whose text goes
+   Tier X Event
+   Battle Y
+   You (win-loss)
+   HP: $HP1
+   Ammo: $bullets1
+   Fatigue: $fatigue1
+   Power: Prep|Catch|Pierce|Aura
+   <hr>
+   P2name
+   $P2block (same as P1)
+   <hr>
+   Round $round
+   <hr>
+
+   then some stuff about previous actions,
+   then <select> elements to input actions, going
+   P1 first move - if P2 reloads - if P2 blocks - if P2 fires 1 to P2 bullets
+   The values are according to action_map below.
+
+   We need to parse the 2 player blocks + the round count, send the
+   resulting game state to the background script then use the response
+   to fill in the selects.
+*/
+var action_map = {
+    "-1": "1^0",
+    "0": "2^0",
+    "1": "3^1",
+    "2": "3^2",
+    "3": "3^3",
+    "4": "3^4",
+    "5": "3^5",
+    "6": "3^6"
+};
+
+var power_map = {
+    "Prep": 0,
+    "Catch": 1,
+    "Pierce": 2,
+    "Aura": 3
+}
+
+function parseGame(elmt) {
+    var txt = elmt.textContent;
+
+    var state = {p1:{}, p2:{}};
+
+    var hp_patt = /HP: (\d)/g;
+    state.p1.hp = parseInt(hp_patt.exec(txt)[1]);
+    state.p2.hp = parseInt(hp_patt.exec(txt)[1]);
+
+    var bullet_patt = /Ammo: (\d)/g;
+    state.p1.bullets = parseInt(bullet_patt.exec(txt)[1]);
+    state.p2.bullets = parseInt(bullet_patt.exec(txt)[1]);
+
+    var fatigue_patt = /Fatigue: (\d)/g;
+    state.p1.fatigue = parseInt(fatigue_patt.exec(txt)[1]);
+    state.p2.fatigue = parseInt(fatigue_patt.exec(txt)[1]);
+
+    var pow_patt = /Power: (\w+)/g;
+    state.p1.type = power_map[pow_patt.exec(txt)[1]];
+    state.p2.type = power_map[pow_patt.exec(txt)[1]];
+
+    var round_patt = /Round (\d+)/;
+    state.round = parseInt(round_patt.exec(txt)[1]);
+
+    return state;
+}
---
+++ src/number_one_winchance.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name           BvS Number One Win Chances
+// @namespace      SkySkimmer
+// @description    Display chance to win Number One games
+// @version        1.0.0
+// @include        http://*.animecubed.com/billy/bvs/numberone.html
+// @include        https://*.animecubed.com/billy/bvs/numberone.html
+// @require        lib.js
+// @resource       wins http://thedragonrider.free.fr/n1wins.txt
+// @grant          GM_getResourceText
+// ==/UserScript==
+
+function doGame(wins, elmt) {
+    var state = parseGame(elmt);
+    var chance = 100 * get_winchance(wins, state);
+
+    // we insert an <hr> and the winchance info before the <hr> which
+    // follows the round number
+    var ref = elmt.getElementsByTagName("hr")[2];
+
+    // put in the <hr>
+    var myhr = document.createElement("hr");
+    ref.parentNode.insertBefore(myhr, ref);
+
+    var winb = document.createElement("b");
+    var wintxt = document.createTextNode("Win: ");
+    ref.parentNode.insertBefore(winb, ref);
+    winb.appendChild(wintxt);
+
+    var chancenode = document.createTextNode("" + chance + "%");
+    ref.parentNode.insertBefore(chancenode, ref);
+}
+
+function main() {
+    var wins = GM_getResourceText("wins");
+
+    var matches = document.forms["maction"].querySelectorAll("td");
+    for (var i = 0; i < matches.length; i++) {
+        doGame(wins, matches[i]);
+    }
+
+}
+
+if(/Your In-Progress Matches/.test(document.body.innerHTML))
+    main();
